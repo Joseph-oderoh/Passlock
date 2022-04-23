ce4e1b75fb430767f2a7840cde0e586238a7592a
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-04-23 12:21:43 +0300

    add save add delete test

--- passlock.py
+++ passlock.py
@@ -1,10 +1,28 @@
+from pyclbr import Class
+
+
 class User:
     """
     This class generates new instance  of user accounts
     """
     user_list = [] # Empty contact list
     def __init__(self,first_name,last_name,password):
-        pass
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)    
+class Credentials:
+    credentials_list = []
+    def __init__(self,account,first_name,last_name,password):
+       
+        self.account = account
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
--- passlock_test.py
+++ passlock_test.py
@@ -16,11 +16,40 @@ class TestUser(unittest.TestCase):
         """
         test init to test if the object was initialized properly
         """    
-        self.assertAlmostEqual(self.new_user.first_name,"Joseph")
-        self.assertAlmostEqual(self.new_user.last_name,"Oderoh")
-        self.assertAlmostEqual(self.new_user.password,"5jko2k1")
+        self.assertEqual(self.new_user.first_name,"Joseph")
+        self.assertEqual(self.new_user.last_name,"Oderoh")
+        self.assertEqual(self.new_user.password,"5jko2k1")
 
-        #! test to let us save our user deta
+        #! test to let us save our user data
+    def test_save_user(self):
+        """
+         test if the users objects  are saved in the user list
+        """
+        self.new_user.save_user() #!saving a new user
+        self.assertEqual(len(User.user_list),1)
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []    
+    def test_save_multiple_user(self):
+        """
+        test_save_multiple_user to check if we can save mutiple users
+        """    
+        self.new_user.save_user()
+        test_user =User("Test","user", "5jko2k1")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+    def test_delete_user(self):
+            '''
+            test_delete_contact to test if we can remove a contact from our contact list
+            '''
+            self.new_contact.save_contact()
+            test_user = User("Test","user","5jko2k1") # new contact
+            test_user.save_user()
+
+            self.new_contact.delete_contact()# Deleting a contact object
+            self.assertEqual(len(Contact.contact_list),1)    
 if __name__ == '__main__':
     unittest.main()    
 
