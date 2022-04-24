5b57e2cb3763fd0db0918522188935275844eff2
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-04-24 10:44:35 +0300

    add display  and login  user functions

--- run.py
+++ run.py
@@ -11,4 +11,25 @@ def save_user(user):
     """
     save a new user
     """
-    user.save_user()    
+    user.save_user()
+def delete_user(user):
+    """
+    function to delet user
+    """
+    user.delete_user()    
+def display_user():
+    """
+    function to display existing user 
+    """
+    return User.display_user() 
+def login_user(first_name,last_name,password) :
+    """
+    function to check if a user exists
+    """
+    check_user  = Credentials.verify_user(first_name,last_name,password)
+    return check_user
+def create_new_credentials(account,usernae,password)
+    """
+    function that creates a new credentials to user account
+    """
+    new_credentials = Credentials(account,username,password)              
